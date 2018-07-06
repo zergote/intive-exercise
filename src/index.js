63502ae9d821d73f77d6b534185f0f826c668d1f
Christian Yanez <xyges@hotmail.com>
2018-07-06 13:58:01 -0400

    Config Webpack and install Webpack-cli and install mini-css-extract-plugin

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./scss/main.scss";
+console.log("hello, world");
+console.log("Hello, world 2");
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// Config for webpack v4
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "style.[chunkhash].css",
+      disable: false,
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ]
+};
